79354804dd66c5abc74873962c4d9e03005782c2
Priyanka8924 <150361778+Priyanka8924@users.noreply.github.com>
2025-03-05 21:50:15 +0530

    Defining and calling the methods in java.

---
+++ methods.java
@@ -0,0 +1,26 @@
+package com.priyanka;
+
+public class methods {
+	public static void main(String[]args) {
+		int a = 30;
+		int b =40;
+		System.out.println(a+b);
+		
+		int value = addition();
+		System.out.println(value);
+		
+		
+	
+	}
+	public static int addition() {
+		int c=40;
+		int d=50;
+		 int x =c+d;
+		 int y = c-d;
+		 
+		
+		
+		return x;
+	}
+
+}
