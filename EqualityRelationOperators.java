7f8bf598d19b85ca74f7c769a5a28050b59a7929
Priyanka8924 <150361778+Priyanka8924@users.noreply.github.com>
2025-03-11 08:10:02 +0530

    Equality and relational operators

---
+++ EqualityRelationOperators.java
@@ -0,0 +1,16 @@
+package com.operators;
+
+public class EqualityRelationOperators {
+    public static void main(String[]args) {
+    	
+    	//Performing the equality and relational operations.
+    	int a =100;
+    	int b= 200;
+    	System.out.println(a>b);
+    	System.out.println(a<b);
+    	System.out.println(a>=b);
+    	System.out.println(a<=b);
+    	System.out.println(a!=b);
+    }
+
+}
