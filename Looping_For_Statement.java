3e5edac16cc6a7546e39fadced4e665c3dab9ab6
Priyanka8924 <150361778+Priyanka8924@users.noreply.github.com>
2025-03-13 07:30:11 +0530

    LOOPING STATEMENT-FOR LOOP.

---
+++ Looping_For_Statement.java
@@ -0,0 +1,35 @@
+package com.controlflowstatement;
+
+public class Looping_For_Statement {
+
+	public static void main(String[] args) {
+		
+		
+	//FOR LOOP
+		
+	     for(int i=333;i<=999;i++)
+		{
+			 if(i%5==0) {
+			 System.out.println(i);
+			 }
+			
+					
+		}
+	     
+	     //when we write print statament in the place of initialization.
+	     int f=0;
+		for(System.out.println("priyanka"),System.out.println("ram");f<=10;f++)
+		{
+			System.out.println(f);
+		}
+		System.out.println("next");
+		
+		//when we write print statament in the place of increment/decrement.
+		
+		for(int z=0;z<=5;System.out.println("priyanka"),System.out.println("ram")) {
+			System.out.println(z);
+			z++;
+		}
+	}
+
+}
